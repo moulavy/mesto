f9d169bab87486b52c43caea2bde27fac8cc88e9
Milana Kulieva <72352010+moulavy@users.noreply.github.com>
2023-01-20 20:39:42 +0300

    убрали лишние bind, сделали стрелочную функцию внутри класса

--- scripts/Popup.js
+++ scripts/Popup.js
@@ -5,14 +5,14 @@ export default class Popup{
    open() {
       
       this._popup.classList.add('popup_opened');
-      document.addEventListener('keydown', this._handleEscClose.bind(this));
+      document.addEventListener('keydown', this._handleEscClose);
    }
    close() {
       this._popup.classList.remove('popup_opened');
-      document.removeEventListener('keydown', this._handleEscClose.bind(this));
+      document.removeEventListener('keydown', this._handleEscClose);
    }
 
-   _handleEscClose(e) {
+   _handleEscClose=(e)=> {
       if (e.key === 'Escape') {
          this.close();
       }   
